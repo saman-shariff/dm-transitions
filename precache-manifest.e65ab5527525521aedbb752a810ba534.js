08481c50d6521eb702329c58a0351d60f72ac8f9
Saman Shariff <saman.shariff@gmail.com>
2023-01-08 20:36:13 +0530

    Updates

---
+++ precache-manifest.e65ab5527525521aedbb752a810ba534.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "82ebec014a565173c468",
+    "url": "/static/css/main.9734c478.chunk.css"
+  },
+  {
+    "revision": "82ebec014a565173c468",
+    "url": "/static/js/main.82ebec01.chunk.js"
+  },
+  {
+    "revision": "382abf35e3d879242896",
+    "url": "/static/js/1.382abf35.chunk.js"
+  },
+  {
+    "revision": "2643322be8430ba0581e",
+    "url": "/static/css/2.b33a8255.chunk.css"
+  },
+  {
+    "revision": "2643322be8430ba0581e",
+    "url": "/static/js/2.2643322b.chunk.js"
+  },
+  {
+    "revision": "600a7b393cddf6c4071d",
+    "url": "/static/js/runtime~main.600a7b39.js"
+  },
+  {
+    "revision": "5598da6a9ad200c0576d6c693075b754",
+    "url": "/static/media/agencyIcon.5598da6a.svg"
+  },
+  {
+    "revision": "c6ef380db4125d159cc6ad8fbf3c6186",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8284f12de901c9a76902f4f503f5bd9c.js"
+  "/precache-manifest.e65ab5527525521aedbb752a810ba534.js"
 );
 
 workbox.clientsClaim();
